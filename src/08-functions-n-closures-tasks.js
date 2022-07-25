2b2f67e4ac6211ac18c0498df37ae83af90207a4
IlyaSolonovich <ilyasolonovich20@gmail.com>
2022-07-25 16:16:09 +0200

    dsa

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -83,8 +83,14 @@ function getPolynom(...arg) {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  const cache = {};
+  return (elem) => {
+    if (cache[elem] === undefined) {
+      cache[elem] = func(elem);
+    }
+    return cache[elem];
+  };
 }
 
 
@@ -149,8 +155,8 @@ function logger(/* func, logFunc */) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return (...args) => fn(...args1, ...args);
 }
 
 
